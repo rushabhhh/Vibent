f28bca60969ae2b744ef27644a63b10b962b53b0
unknown <rushabhrathod018@gmail.com>
2025-08-22 16:45:34 +0530

    Updated org

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -8,7 +8,6 @@ import { bsc, bscTestnet } from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider, ConnectButton, darkTheme } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import '@rainbow-me/rainbowkit/styles.css'
 
 const queryClient = new QueryClient();
 
